514f0e0ee47a280561a02d94d2556c7f3415ef85
Alex Koppel <alex@alexkoppel.com>
2015-05-24 18:36:46 -0500

    Add dashes to word delimiters

--- lib/markov_chain/text_splitter.rb
+++ lib/markov_chain/text_splitter.rb
@@ -8,7 +8,7 @@ module MarkovChain
     # word delimiters -- spaces, quotes, (various symbols followed by either another dash, a space,
     # another dot, or the end of the text), or (colons preceded by space or the beginning of the
     # text). we don't want to block things like Jones-Smith, tl;dr, abc.def, or it's.
-    WORD_DELIMITERS = /([\s\(\)]|"|[\.-:;\?\!]([-\.\s]|$)|[\s^]')/
+    WORD_DELIMITERS = /([\s\(\)\-]|"|[\.-:;\?\!]([-\.\s]|$)|[\s^]')/
 
     # anything that doesn't contain any letters is not a word we need to care about
     MARKERS_OF_INTEREST = /[A-Za-z]/
