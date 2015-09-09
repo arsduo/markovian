0a8e55fc9507b6c650aad4f72a5f9143b9447f4d
Alex Koppel <alex@alexkoppel.com>
2015-09-08 23:21:40 -0500

    Automatically include all files needed to build text

--- lib/markovian.rb
+++ lib/markovian.rb
@@ -1,6 +1,9 @@
 require 'oj'
 require 'markovian/chain_set'
 require 'markovian/text_builder'
+require 'markovian/chain/text_compiler'
+# importers
+require 'markovian/importers/twitter/csv_importer'
 
 # The base module.
 module Markovian
